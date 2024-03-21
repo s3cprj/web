4f1ee6e1b7a702d68dab4fd5c4a3553b433025c2
takato256 <c1139952@planet.kanazawa-it.ac.jp>
2024-03-21 13:58:53 +0900

    first commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+// ページが読み込まれたときにHomeタブを自動的に表示
+document.addEventListener('DOMContentLoaded', (event) => {
+    document.querySelector('.tablink').click();
+});
